9cd6e6fdfb781124c6f0332708f7583503d75091
wuw-sh <wuw6248@gmail.com>
2023-07-12 17:47:20 +0700

    ✅ [@] wuw.sh

--- pack-script-init.bat
+++ pack-script-init.bat
@@ -1,5 +1,7 @@
 @echo off
-call npm i @minecraft/server@1.4.0-beta.1.20.10-stable
+
+if not exist node_modules/@minecraft/server call npm i @minecraft/server@1.4.0-beta.1.20.10-stable
+if not exist node_modules/@minecraft/server-ui call npm i @minecraft/server-ui@1.2.0-beta.1.20.10-stable
 
 (
     echo {
@@ -23,3 +25,16 @@ call npm i @minecraft/server@1.4.0-beta.1.20.10-stable
     echo @echo off
     echo call tsc -w
 ) > TS-compiler.bat
+
+(
+    echo node_modules/
+    echo package.json
+    echo package-lock.json
+    echo tsconfig.json
+    echo TS-compiler.bat
+    echo .vscode/
+) > .gitignore
+
+call mkdir src
+cd src
+call mkdir server
